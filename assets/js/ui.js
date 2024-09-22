7303d8cd214c0cdca2d430f33d325ae60aed9861
sisogame <107414791+sisogame@users.noreply.github.com>
2024-09-23 01:03:57 +0900

    multu modal

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    
+
+
     document.addEventListener('DOMContentLoaded',
         function () {
             // 20240720 add script - ley
@@ -16,6 +17,51 @@ $(document).ready(function () {
             });
         });
 
-   
 
+
+});
+
+//팝업
+$(function () {
+    var target = document.querySelectorAll('.pop-open');
+    var btnPopClose01 = document.querySelectorAll('.pop-close01');
+    var btnPopClose02 = document.querySelectorAll('.pop-close02');
+    var targetID;
+
+    // 팝업 열기
+    for (var i = 0; i < target.length; i++) {
+        target[i].addEventListener('click', function () {
+            targetID = this.getAttribute('href');
+            document.querySelector(targetID).style.display = 'block';
+        });
+    }
+
+
+});
+
+//팝업 닫기
+function dEI(elementID) {
+    return document.getElementById(elementID);
+}
+
+function closeLayer(IdName) {
+    var pop = dEI(IdName);
+    pop.style.display = "none";
+
+}
+
+
+// $(function () {
+// let $modal = $("#pop-search01");
+// $modal.draggable({
+//     handle: ".modal-header",
+// });
+//     $modal.resizable();
+
+// });
+
+
+$(function () {
+    $("#draggable").draggable();
+    $("#draggable02").draggable();
 });
