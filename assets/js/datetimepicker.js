54c27e8b6582e2049685312417bfd098c93e7291
sisogame <107414791+sisogame@users.noreply.github.com>
2024-10-06 20:23:35 +0900

    폴더별 화면 구성

--- assets/js/datetimepicker.js
+++ assets/js/datetimepicker.js
@@ -57,23 +57,72 @@ $(function () {
   });
 
 
-    $("#datebasicEnd").datetimepicker({
-      lang: "kr",
-      timepicker: false,
-      format: "Y/m/d",
-      formatDate: "Y/m/d",
-    });
+  $("#datebasicEnd").datetimepicker({
+    lang: "kr",
+    timepicker: false,
+    format: "Y/m/d",
+    formatDate: "Y/m/d",
+  });
 
-    $("#open").click(function () {
-      $("#datebasicEnd").datetimepicker("show");
-    });
+  $("#open").click(function () {
+    $("#datebasicEnd").datetimepicker("show");
+  });
+
+  // 달력-년월일#2 
+  $("#datebasicStart2").datetimepicker({
+    lang: "kr",
+    timepicker: false,
+    format: "Y/m/d",
+    formatDate: "Y/m/d",
+  });
+
+  $("#open").click(function () {
+    $("#datebasicStart2").datetimepicker("show");
+  });
+
+
+  $("#datebasicEnd2").datetimepicker({
+    lang: "kr",
+    timepicker: false,
+    format: "Y/m/d",
+    formatDate: "Y/m/d",
+  });
+
+  $("#open").click(function () {
+    $("#datebasicEnd2").datetimepicker("show");
+  });
+
+  // 달력-년월일#3 
+  $("#datebasicStart3").datetimepicker({
+    lang: "kr",
+    timepicker: false,
+    format: "Y/m/d",
+    formatDate: "Y/m/d",
+  });
+
+  $("#open").click(function () {
+    $("#datebasicStart3").datetimepicker("show");
+  });
+
+
+  $("#datebasicEnd3").datetimepicker({
+    lang: "kr",
+    timepicker: false,
+    format: "Y/m/d",
+    formatDate: "Y/m/d",
+  });
+
+  $("#open").click(function () {
+    $("#datebasicEnd3").datetimepicker("show");
+  });
+
 
 });
 
 $(function () {
-// Appending the timepicker element to the body or a container
+  // Appending the timepicker element to the body or a container
 
 
 
 
-  });
+});
